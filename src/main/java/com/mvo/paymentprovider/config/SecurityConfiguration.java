389fa9aefd5c22cadb6db4303323130b07d8f8cf
Vlad_Milas <12milash12@gmail.com>
2024-09-13 15:17:57 +0300

    started implements authenticate

---
+++ src/main/java/com/mvo/paymentprovider/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.mvo.paymentprovider.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.server.session.WebSessionManager;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults())
+                .sessionManagement(sessionManagement -> sessionManagement
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+        return http.build();
+    }
+
+}
