0e80cdfb1a13bbe90f0c5a5a669a8a5f1a62ab6f
Vlad_Milas <12milash12@gmail.com>
2024-09-15 10:43:26 +0300

    refactored authenticate

--- src/main/java/com/mvo/paymentprovider/config/SecurityConfiguration.java
+++ src/main/java/com/mvo/paymentprovider/config/SecurityConfiguration.java
@@ -2,35 +2,34 @@ package com.mvo.paymentprovider.config;
 
 import com.mvo.paymentprovider.security.MerchantDetailService;
 import lombok.RequiredArgsConstructor;
-import org.postgresql.shaded.com.ongres.scram.common.bouncycastle.base64.Base64;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
 import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import java.util.Base64;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
     private final MerchantDetailService merchantDetailService;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .anyRequest().authenticated()
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .anyExchange().authenticated()
                 )
                 .httpBasic(withDefaults())
-                .sessionManagement(sessionManagement -> sessionManagement
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-        return http.build();
+                .build();
     }
 
     @Bean
@@ -50,15 +49,14 @@ public class SecurityConfiguration {
 
         @Override
         public String encode(CharSequence rawPassword) {
-            return Base64.toBase64String(rawPassword.toString().getBytes());
+            return Base64.getEncoder().encodeToString(rawPassword.toString().getBytes());
         }
 
         @Override
         public boolean matches(CharSequence rawPassword, String encodedPassword) {
-            String encodeRawPassword = Base64.toBase64String(rawPassword.toString().getBytes());
+            String encodeRawPassword = encode(rawPassword);
             return encodedPassword.equals(encodeRawPassword);
         }
     }
-
 
 }
